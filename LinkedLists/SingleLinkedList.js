f3253c0f88008ee6b9d5eb71862ff972e70f402a
SenthilBalaji <senthilbalaji@SenthilBalajis-MacBook-Air.local>
2019-04-13 15:38:33 +0530

    added inserting node, before, after and given position

--- LinkedLists/SingleLinkedList.js
+++ LinkedLists/SingleLinkedList.js
@@ -2,7 +2,7 @@ class SingleLinkedList {
     constructor() {
         this.start = undefined;
     }
-    
+
     displayList() {
         if (!this.start) console.log("list is empty");
         let singleNode = this.start;
@@ -33,6 +33,80 @@ class SingleLinkedList {
             singleNode = singleNode.next_node;
         }
         console.log("Value not Found");
+    }
+
+    insertInBeginning(node) {
+        node.next_node = this.start;
+        this.start = node;
+    }
+
+    insertInEnd(node) {
+        let singleNode = this.start;
+        if (!singleNode) {
+            this.start = node;
+            return;
+        }
+        while (singleNode.next_node) {
+            singleNode = singleNode.next_node;
+        }
+        singleNode.next_node = node;
+    }
+
+    insertAfterNode(beforeNodeInfo, nextNode) {
+        if (!this.start) {
+            console.log("linked list is empty");
+            return;
+        }
+        let singleNode = this.start;
+        while (singleNode) {
+            if (singleNode.info == beforeNodeInfo) {
+                nextNode.next_node = singleNode.next_node;
+                singleNode.next_node = nextNode;
+                return;
+            }
+            singleNode = singleNode.next_node;
+        }
+        console.log("no node with value is found");
+    }
+
+    insertBeforeNode(afterNodeInfo, node) {
+        if (!this.start) {
+            console.log("linked list is empty");
+            return;
+        }
+        let singleNode = this.start;
+        if (singleNode.info == afterNodeInfo) { 
+            this.insertInBeginning(node);
+            return;
+        }
+        while (singleNode.next_node) {
+            if (singleNode.next_node.info == afterNodeInfo) {
+                node.next_node = singleNode.next_node;
+                singleNode.next_node = node;
+                return;
+            }
+            singleNode = singleNode.next_node;
+        }
+        console.log("no node with value is found");
+    }
+
+    insertAtPosition(position, node) {
+        let curPos = 1;
+        if (position == 1) {
+            this.insertInBeginning(node);
+            return;
+        }
+        let singleNode = this.start;
+        while (singleNode) {
+            if (curPos == position - 1) {
+                node.next_node = singleNode.next_node;
+                singleNode.next_node = node;
+                return;
+            }
+            curPos++;
+            singleNode = singleNode.next_node;
+        }
+        console.log("list is not having less number of nodes");
     }
 }
 
--- index.js
+++ index.js
@@ -5,9 +5,16 @@ sl1 = new SingleLinkedList();
 node1 = new Node(1);
 node2 = new Node(2);
 node3 = new Node(3);
+node4 = new Node(4);
+node5 = new Node(5);
 sl1.start = node1;
 node1.next_node = node2;
 node2.next_node = node3;
-// sl1.displayList();
 // sl1.countNodes();
-sl1.search(7);
+// sl1.search(7);
+// sl1.insertInBeginning(node4);
+// sl1.insertInEnd(node4);
+// sl1.insertAfterNode(3, node5);
+// sl1.insertBeforeNode(1, node5)
+// sl1.insertAtPosition(1, node4);
+sl1.displayList();
