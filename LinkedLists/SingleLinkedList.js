d7739fb2af0b1500185c304a3f1f0375166a4840
SenthilBalaji <senthilbalaji@SenthilBalajis-MacBook-Air.local>
2019-04-14 17:25:53 +0530

    added bubble sort for linked list exchange data and exchange links

--- LinkedLists/SingleLinkedList.js
+++ LinkedLists/SingleLinkedList.js
@@ -19,7 +19,7 @@ class SingleLinkedList {
             singleNode = singleNode.next_node;
             count += 1;
         }
-        console.log(count);
+        return count;
     }
 
     search(value) {
@@ -165,6 +165,119 @@ class SingleLinkedList {
                 singleNode.next_node = singleNode.next_node.next_node
             }
             singleNode = singleNode.next_node;
+        }
+    }
+
+    reverseList() {
+        let prev = undefined;
+        let singleNode = this.start;
+        while (singleNode) {
+            let next_node = singleNode.next_node;
+            singleNode.next_node = prev;
+            prev = singleNode;
+            singleNode = next_node;
+        }
+        this.start = prev;
+    }
+
+    bubbleSortListExchangeData() {
+        if (!this.start) {
+            console.log("list is empty");
+            return;
+        }
+        let loopTimes = this.countNodes() - 1;
+        for (let i = 0; i < loopTimes; i++) {
+            let singleNode = this.start;
+            while (singleNode.next_node) {
+                let next_node = singleNode.next_node;
+                if (singleNode.info > next_node.info) {
+                    let temp = singleNode.info;
+                    singleNode.info = next_node.info;
+                    next_node.info = temp;
+                }
+                singleNode = singleNode.next_node;
+            }
+        }
+    }
+
+    bubbleSortListExchangeDataNoCount() {
+        if (!this.start) {
+            console.log("list is empty");
+            return;
+        }
+        let end = undefined;
+        while (end != this.start.next_node) {
+            let singleNode = this.start;
+            while (singleNode.next_node != end) {
+                let next_node = singleNode.next_node;
+                if (singleNode.info > next_node.info) {
+                    let temp = singleNode.info;
+                    singleNode.info = next_node.info;
+                    next_node.info = temp;
+                }
+                singleNode = singleNode.next_node;
+            }
+            end = singleNode;
+        }
+    }
+
+    // bubbleSortListExchangeLink() {
+    //     if (!this.start) {
+    //         console.log("list is empty");
+    //         return;
+    //     }
+    //     let end = undefined;
+    //     while (end != this.start.next_node) {
+    //         let singleNode = this.start;
+    //         let prev_node = this.start;
+    //         while (end != singleNode.next_node) {
+    //             let next_node = singleNode.next_node;
+    //             if (singleNode.info > next_node.info) {
+    //                 singleNode.next_node = next_node.next_node;
+    //                 next_node.next_node = singleNode;
+    //                 if (this.start == singleNode) {
+    //                     this.start = next_node;
+    //                 } else {
+    //                     prev_node.next_node = next_node;
+    //                 }
+    //                 next_node = [singleNode, singleNode = next_node][0];
+    //                 this.displayList();
+    //                 console.log("============")
+    //             }
+    //             prev_node = singleNode;
+    //             singleNode = singleNode.next_node;
+    //         }
+    //         end = singleNode;
+    //     }
+    // }
+
+    bubbleSortListExchangeLink() {
+        if (!this.start) {
+            console.log("list is empty");
+            return;
+        }
+        let end = undefined;
+        while (end != this.start.next_node) {
+            let singleNode = this.start;
+            let next_node = singleNode.next_node;
+            let prev_node = undefined;
+            while (next_node != end) {
+                if (singleNode.info > next_node.info) {
+                    singleNode.next_node = next_node.next_node;
+                    next_node.next_node = singleNode;
+                    if (!prev_node) {
+                        this.start = next_node;
+                    } else {
+                        prev_node.next_node = next_node;
+                    }
+                    prev_node = next_node;
+                } else {
+                    prev_node = singleNode;
+                    singleNode = singleNode.next_node;
+                }
+                next_node = singleNode.next_node;
+            }
+            end = singleNode;
         }
     }
 }
